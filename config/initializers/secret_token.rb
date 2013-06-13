5d86095cc23a6b6a6ef64fe470a6cbfc714dff93
raynoe <raynoe_lxd@hotmail.com>
2013-06-13 13:28:33 +0700

    first create app for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorummi::Application.config.secret_token = 'f04a997339387ccc7a4894f17d7fa1a3b1a8b075b85cf78760d74db9a279c7483050632142ebe7361f6c048eb2d800b98d2bd8eb43c7586c1b0e0a57a215c05e'
