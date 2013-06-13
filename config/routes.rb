8fa1f0e5d8135a587d482a38dbf0deb6dc6e5efe
raynoe <raynoe_lxd@hotmail.com>
2013-06-13 23:12:51 +0700

    add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rorummi::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
+  ActiveAdmin.routes(self)
 
   resources :forums
   resources :votes
